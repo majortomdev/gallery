b77856b7b5e67669b5551456d437a3738ad56135
Joseph Kinahan <kinahanj25@gmail.com>
2019-09-23 23:37:54 +0100

    added some routes and played with some style binding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
 
+//component with ' '(empty) path is the one that loads first
+const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'list', component: ListComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
