a6e2b4be8b2c6d8447fbae6b0c4c466e92394513
Joseph Kinahan <kinahanj25@gmail.com>
2019-09-29 22:15:04 +0100

    +new book comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
 
 //component with ' '(empty) path is the one that loads first
 const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'list', component: ListComponent},
+    { path: 'books', component: BooksComponent}
 
 ];
 
